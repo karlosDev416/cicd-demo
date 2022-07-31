ebba5fbde0bf075ee97e0ea7ba04cf05e31d6190
Karlos Aguirre <juan.aguirre@bigtoken.com>
2022-07-31 16:26:00 -0500

    [ADD] Initial project

---
+++ cicd-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cicd-demo
+//
+//  Created by KARLOS AGUIRRE on 7/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .green
+    }
+
+
+}
+
